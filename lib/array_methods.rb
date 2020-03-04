befb6a03717dbf4d9e9bf536c1802a5c8f76c70e
George Cooper <coopsgeorge@gmail.com>
2020-03-04 12:45:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,9 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  array.length.times do |count|
+    if array[count] = value to find
+      return count
+    end 
+    nil 
 end
 
 
